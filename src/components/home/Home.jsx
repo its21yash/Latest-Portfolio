01b8e5aef95ed04a474f8bb333823f7926c4766e
Yashpal <yash38709@gmail.com>
2024-09-27 00:34:46 +0530

    Updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="home__container container grid">
         <div className="home__content grid">
           <Social />
-          <img src="/assets/profile1.webp" alt="" className="home__img" />
+          <img src="/assets/profile1.webp" alt="" className="home__img " />
           {/* <div className="home__img"></div> */}
           <Data />
         </div>
