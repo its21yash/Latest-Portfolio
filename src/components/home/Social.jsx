57e827f0b922e4098d86b6a776dfc22314c8e73e
Yashpal <yash38709@gmail.com>
2024-12-15 19:44:54 +0530

    Resume Updated

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,12 +4,12 @@ const Social = () => {
   return (
     <div className="home__social">
       <a
-        href="https://github.com/yash2026"
+        href="https://github.com/its21yash"
         className="home__social-icon"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i class="uil uil-github-alt"></i>
+        <i className="uil uil-github-alt"></i>
       </a>
       <a
         href="https://www.linkedin.com/in/yashpal-kumar-9b785b1a6/"
@@ -17,7 +17,7 @@ const Social = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i class="uil uil-linkedin-alt"></i>
+        <i className="uil uil-linkedin-alt"></i>
       </a>
       <a
         href="https://twitter.com/its21yash"
@@ -25,7 +25,7 @@ const Social = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i class="uil uil-twitter-alt"></i>
+        <i className="uil uil-twitter-alt"></i>
       </a>
       <a
         href="https://www.instagram.com/its21yash/"
@@ -33,7 +33,7 @@ const Social = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i class="uil uil-instagram"></i>
+        <i className="uil uil-instagram"></i>
       </a>
     </div>
   );
